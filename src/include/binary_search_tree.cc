b5bf2d1c3c4e8475c98429d990db461d598bf6da
myqway <myqway@outlook.com>
2016-04-29 19:06:47 +0800

    use template

--- src/include/binary_search_tree.cc
+++ src/include/binary_search_tree.cc
@@ -1,130 +1,144 @@
 #include "binary_search_tree.h"
 namespace MySTL {
-	BinarySearchTree::node* BinarySearchTree::FindMin(node* p) {
-		if(nullptr == p)
-			return nullptr;
 
-		while(p->left) {
+template <class T>
+typename BinarySearchTree<T>::node* BinarySearchTree<T>::FindMin(node* p) {
+	if(nullptr == p)
+		return nullptr;
+
+	while(p->left) {
+		p = p->left;
+	}
+	return p;
+}
+
+template<class T>
+typename BinarySearchTree<T>::node* BinarySearchTree<T>::FindMax(node* p) {
+	if(nullptr == p)
+		return nullptr;
+
+	while(p->right) {
+		p = p->left;
+	}
+	return p;
+}
+
+template<class T>
+typename BinarySearchTree<T>::node* BinarySearchTree<T>::FindValue(T val, node* p) {
+	while(nullptr != p) {
+		if (val < p->data)
 			p = p->left;
+		else if (val > p->data)
+			p = p->right;
+		else {
+			break;
 		}
-		return p;
 	}
+	return p;
+}
 
-	BinarySearchTree::node* BinarySearchTree::FindMax(node* p) {
-		if(nullptr == p)
-			return nullptr;
+template<class T>
+void BinarySearchTree<T>::InsertPvt(T val, node* &ptr) {
+	if(nullptr == ptr) {
+		ptr = new node(val);
+		size_++;
+	} else if (val < ptr->data) {
+		InsertPvt(val, ptr->left);
 
-		while(p->right) {
-			p = p->left;
+	} else if (val > ptr->data) {
+		InsertPvt(val, ptr->right);
+
+	} else {
+		return;
+	}
+}
+
+template<class T>
+void BinarySearchTree<T>::ErasePvt(T val, node* &ptr) {
+	if (ptr == 0)
+		return;
+	if (val < ptr->data)
+		return ErasePvt(val, ptr->left);
+	else if (val > ptr->data)
+		return ErasePvt(val, ptr->right);
+	else{ // found
+		if (ptr->left != 0 && ptr->right != 0){// has two children
+			size_t choose = size_ % 2;
+			//random to erase left or right
+			auto pos = choose == 0 ? FindMin(ptr->right) : FindMax(ptr->left);
+			ptr->data = pos->data;
+			return (choose == 0 ? ErasePvt(pos->data, ptr->right) : ErasePvt(pos->data, ptr->left));
+		} else{ //has one or no child
+			auto temp = ptr;
+			if (nullptr != ptr->left)
+				ptr = ptr->left;
+			else
+				ptr = ptr->right;
+			delete temp; // segmentation fault: because err: node* ptr shold be: node* &ptr
+			temp = nullptr;
+			--size_;
 		}
-		return p;
 	}
+}
 
-	BinarySearchTree::node* BinarySearchTree::FindValue(value_type val, node* p) {
-		while(nullptr != p) {
-			if (val < p->data)
-				p = p->left;
-			else if (val > p->data)
-				p = p->right;
-			else {
-				break;
-			}
-		}
-		return p;
-	}
-
-	void BinarySearchTree::InsertPvt(value_type val, node* &ptr) {
-		if(nullptr == ptr) {
-			ptr = new node(val);
-			size_++;
-		} else if (val < ptr->data) {
-			InsertPvt(val, ptr->left);
-
-		} else if (val > ptr->data) {
-			InsertPvt(val, ptr->right);
-
+template<class T>
+void BinarySearchTree<T>::DeleteBST(node *ptr) {
+	if(ptr) {
+		DeleteBST(ptr->left);
+		DeleteBST(ptr->right);
+		if(nullptr != ptr) {
+			delete ptr;
+			ptr = nullptr;
 		} else {
 			return;
 		}
 	}
+}
 
-	void BinarySearchTree::ErasePvt(value_type val, node* &ptr) {
-		if (ptr == 0)
-			return;
-		if (val < ptr->data)
-			return ErasePvt(val, ptr->left);
-		else if (val > ptr->data)
-			return ErasePvt(val, ptr->right);
-		else{ // found
-			if (ptr->left != 0 && ptr->right != 0){// has two children
-				size_t choose = size_ % 2;
-				//random to erase left or right
-				auto pos = choose == 0 ? FindMin(ptr->right) : FindMax(ptr->left);
-				ptr->data = pos->data;
-				return (choose == 0 ? ErasePvt(pos->data, ptr->right) : ErasePvt(pos->data, ptr->left));
-			} else{ //has one or no child
-				auto temp = ptr;
-				if (nullptr != ptr->left)
-					ptr = ptr->left;
-				else
-					ptr = ptr->right;
-				delete temp; // segmentation fault: because err: node* ptr shold be: node* &ptr
-				temp = nullptr;
-				--size_;
-			}
-		}
+template<class T>
+size_t BinarySearchTree<T>::HeightPvt(node *ptr) {
+	size_t height_l = 0; 
+	size_t height_r = 0;
+	if(ptr->left){
+		height_l = 1 + HeightPvt(ptr->left);
 	}
 
-	void BinarySearchTree::DeleteBST(node *ptr) {
-		if(ptr) {
-			DeleteBST(ptr->left);
-			DeleteBST(ptr->right);
-			if(nullptr != ptr) {
-				delete ptr;
-				ptr = nullptr;
-			} else {
-				return;
-			}
-		}
+	if(ptr->right){
+		height_r = 1 + HeightPvt(ptr->right);
 	}
 
-	size_t BinarySearchTree::HeightPvt(node *ptr) {
-		size_t height_l = 0; 
-		size_t height_r = 0;
-		if(ptr->left){
-			height_l = 1 + HeightPvt(ptr->left);
-		}
+	return height_l > height_r ? height_l : height_r;
+}
 
-		if(ptr->right){
-			height_r = 1 + HeightPvt(ptr->right);
-		}
+template<class T>
+void BinarySearchTree<T>::PrintInorderPvt(node * ptr) {
+	if (ptr) {
+		PrintInorderPvt(ptr->left);
+		std::cout << ptr->data << "\n";
+		PrintInorderPvt(ptr->right);
+	}
+}
 
-		return height_l > height_r ? height_l : height_r;
+template<class T>
+void BinarySearchTree<T>::PrintPreorderPvt(node *ptr) {
+	if (ptr) {
+		std::cout << ptr->data << "\n";
+		PrintInorderPvt(ptr->left);
+		PrintInorderPvt(ptr->right);
 	}
+}
 
-	void BinarySearchTree::PrintInorderPvt(node * ptr) {
-		if (ptr) {
-			PrintInorderPvt(ptr->left);
-			std::cout << ptr->data << "\n";
-			PrintInorderPvt(ptr->right);
-		}
+template<class T>
+void BinarySearchTree<T>::PrintPostorderPvt(node *ptr) {
+	if (ptr) {
+		PrintInorderPvt(ptr->left);
+		PrintInorderPvt(ptr->right);
+		std::cout << ptr->data << "\n";
 	}
-	void BinarySearchTree::PrintPreorderPvt(node *ptr) {
-		if (ptr) {
-			std::cout << ptr->data << "\n";
-			PrintInorderPvt(ptr->left);
-			PrintInorderPvt(ptr->right);
-		}
-	}
-	void BinarySearchTree::PrintPostorderPvt(node *ptr) {
-		if (ptr) {
-			PrintInorderPvt(ptr->left);
-			PrintInorderPvt(ptr->right);
-			std::cout << ptr->data << "\n";
-		}
-	}
+}
 
-	BinarySearchTree::~BinarySearchTree() {
-		DeleteBST(root_);
-	}
+template<class T>
+BinarySearchTree<T>::~BinarySearchTree() {
+	DeleteBST(root_);
+}
 }
--- src/include/binary_search_tree.h
+++ src/include/binary_search_tree.h
@@ -1,46 +1,51 @@
 #include <cstddef>
 #include <iostream>
+#include <string>
 namespace MySTL {
-	typedef int value_type;
+template<class T>
+class BinarySearchTree {
+public:
+	//typedef int value_type;
+	//typedef std::string value_type;
+	typedef T value_type;
 
-	class BinarySearchTree {
-	public:
-		struct node {
-			value_type data;
+	struct node {
+		T data;
 
-			node *left;
-			node *right; 
-			explicit node(value_type mydata, node *myleft = nullptr, node *myright =nullptr)
-						 :data(mydata), left(myleft), right(myright){}
-		};
+		node *left;
+		node *right;
 
-	private:
-		node * root_;
-		size_t size_; // cnt of node
-
-	public:
-		BinarySearchTree(): root_(nullptr), size_(0){};
-		BinarySearchTree(const BinarySearchTree&) = delete;
-		BinarySearchTree& operator=(const BinarySearchTree&) = delete;
-		~BinarySearchTree();
-
-		void Insert(value_type val) {return InsertPvt(val, root_);}
-		void Erase(value_type val) {return ErasePvt(val, root_);}
-		size_t Height() { return HeightPvt(root_); }
-		void PrintInorder(){ return PrintInorderPvt(root_); }
-		void PrintPreorder(){ return PrintPreorderPvt(root_); }
-		void PrintPostorder(){ return PrintPostorderPvt(root_); }
-	private:
-		node* FindMin(node*);
-		node* FindMax(node*);
-		node* FindValue(value_type, node*);
-		size_t HeightPvt(node *);
-
-		void DeleteBST(node *ptr);
-		void InsertPvt(value_type val, node* &ptr);
-		void ErasePvt(value_type val, node* &ptr);
-		void PrintInorderPvt(node *ptr);
-		void PrintPreorderPvt(node *ptr);
-		void PrintPostorderPvt(node *ptr);
+		explicit node(T mydata, node *myleft = nullptr, node *myright =nullptr)
+					 :data(mydata), left(myleft), right(myright){}
 	};
+
+private:
+	node * root_;
+	size_t size_; // cnt of node
+
+public:
+	BinarySearchTree(): root_(nullptr), size_(0){};
+	BinarySearchTree(const BinarySearchTree&) = delete;
+	BinarySearchTree& operator=(const BinarySearchTree&) = delete;
+	~BinarySearchTree();
+
+	void Insert(value_type val) {return InsertPvt(val, root_);}
+	void Erase(value_type val) {return ErasePvt(val, root_);}
+	size_t Height() { return HeightPvt(root_); }
+	void PrintInorder(){ return PrintInorderPvt(root_); }
+	void PrintPreorder(){ return PrintPreorderPvt(root_); }
+	void PrintPostorder(){ return PrintPostorderPvt(root_); }
+private:
+	node* FindMin(node*);
+	node* FindMax(node*);
+	node* FindValue(value_type, node*);
+	size_t HeightPvt(node *);
+
+	void DeleteBST(node *ptr);
+	void InsertPvt(value_type val, node* &ptr);
+	void ErasePvt(value_type val, node* &ptr);
+	void PrintInorderPvt(node *ptr);
+	void PrintPreorderPvt(node *ptr);
+	void PrintPostorderPvt(node *ptr);
+};
 }
